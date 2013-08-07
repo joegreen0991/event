3102394a05d79f0b563749186d2842ba327aa5fc
joegreen0991 <joe.green0991@gmail.com>
2013-08-07 13:01:03 +0100

    Create event.js

---
+++ event.js
@@ -0,0 +1,101 @@
+/**
+   * Cross browser event handling
+	 */
+
+	var Event = function(window,document){
+
+		var Cache = {
+			cache : {},
+			store : function( elem, type, handler ){
+				if(!Cache.cache[type]){
+					Cache.cache[type] = [];
+				}
+				Cache.cache[type].push([elem, type]);
+			},
+			get : function( elem, type ){
+				if(!Cache.cache[type]){
+					return false;
+				}
+				for(var i = 0; i < Cache.cache.length; i++){
+					if(Cache.cache[type][i][0] === elem) return type;
+				}
+			}
+		};
+
+		var Exports = {
+			unbind : function() {  
+				if ( document.removeEventListener ) {  
+					return function( elem, type ) {  
+						if ( (elem && !elem.length) || elem === window ) {  
+							var handler = Cache.get(elem, type);
+							elem.removeEventListener(type, handler, false );  
+						}  
+						else if ( elem && elem.length ) {  
+							var len = elem.length;  
+							for ( var i = 0; i < len; i++ ) {  
+								Exports.removeEvent( elem[i], type );  
+							}  
+						}  
+					};  
+				}  
+				else if ( document.detachEvent ) {  
+					return function ( elem, type ) {  
+						if ( (elem && !elem.length) || elem === window ) {  
+							var handler = Cache.get(elem, type);
+							elem.detachEvent( 'on' + type, handler );  
+						}  
+						else if ( elem.length ) {  
+							var len = elem.length;  
+							for ( var i = 0; i < len; i++ ) {  
+								Exports.removeEvent( elem[i], type );  
+							}  
+						}  
+					};  
+				}  
+			}(),
+
+			bind : function() {  
+				if ( document.addEventListener ) {  
+					return function( elem, type, handler ) {  
+						if ( (elem && !elem.length) || elem === window ) { 
+							Cache.store( elem, type, handler );
+							elem.addEventListener(type, handler, false );  
+						}  
+						else if ( elem && elem.length ) {  
+							var len = elem.length;  
+							for ( var i = 0; i < len; i++ ) {  
+								Exports.addEvent( elem[i], type, handler );  
+							}  
+						}  
+					};  
+				}  
+				else if ( document.attachEvent ) {  
+					return function ( elem, type, handler ) {  
+						if ( (elem && !elem.length) || elem === window ) {  
+							Cache.store( elem, type, handler );
+							elem.attachEvent( 'on' + type, function() { return handler.call(elem, window.event) } );  
+						}  
+						else if ( elem.length ) {  
+							var len = elem.length;  
+							for ( var i = 0; i < len; i++ ) {  
+								Exports.addEvent( elem[i], type, handler );  
+							}  
+						}  
+					};  
+				}  
+			}()
+		};
+
+		var bindings = ['blur','change','click','dblclick','focus','keydown','keypress','keyup','load','mousedown','mouseenter','mouseleave','mouseover','mouseout','mouseup','resize','scroll','submit'];
+
+		for(var i = 0; i < bindings.length; i++){
+			(function(i){
+				Exports[bindings[i]] = function(element,handler){
+					return Exports.bind(element, bindings[i],handler);
+				};
+			})(i);
+		}
+
+		return Exports;
+
+	}(window,document);
